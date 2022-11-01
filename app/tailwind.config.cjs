b253ada8fbd598a65ec87da8d35019b4f35e3cce
Peetu Saarinen <root@chargedbyte.dev>
2022-11-01 20:11:50 +0200

    Initial commit

---
+++ app/tailwind.config.cjs
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{cjs,html,js,mjs,svelte,ts}"],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
